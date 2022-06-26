c18452928f89d1207b28a725f38c1258f5f173fc
Kayla McDanel <ks.mcdanel@gmail.com>
2022-06-26 14:31:59 -0400

    week 5 add

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-//route to landing and grooming pages
+const express = require("express");
 
-var express = require('express');
-var app = express();
 
-app.get ("/index.html", function(request, response){
-    response.send("Welcome to Pets-R-Us!");
+const app = express();
+const port = 3000;
+
+//Static Files
+app.use(express.static("public"));
+app.use("/images", express.static(__dirname + "public/images"));
+app.use("/styles", express.static(__dirname + "public/styles"));
+app.use("/styles/site.css", express.static(__dirname + "public/styles/site.css"));
+
+//Sets Express Layouts and overrides default directory
+
+
+//HTML Routes
+app.set("view engine", "ejs");
+app.engine(".html", require("ejs").__express);
+app.set("views", "./views");
+
+app.get("", (req, res) => {
+  res.render("index.html");
 });
 
-app.get ("/grooming.html", function(request, response){
-    response.send("Let us brush your dog!");
+app.get("/grooming", (req, res) => {
+  res.render("grooming.html");
 });
 
-app.use (function(request, response){
-    response.status(404) .send("Page not found!");
+app.get("/boarding", (req, res) => {
+  res.render("boarding.html");
+});
+
+app.get("/registration", (req, res) => {
+  res.render("registration.html");
+});
+
+app.get("/training", (req, res) => {
+  res.render("training.html");
+});
+
+//Listen on Port 3000
+
+app.listen(port, () => {
+  console.log("Application started and listening on port" + port);
 });
